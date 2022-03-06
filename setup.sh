f5ae8bc83d1b94bc9dc659c1709176d0f6cf9eb6
Elliott Magnuson <ecmagnuson@wisc.edu>
2022-03-05 18:17:17 -0600

    working on getting it to export variable

--- setup.sh
+++ setup.sh
@@ -3,13 +3,15 @@
 #If this horribly pains you to put in Home then feel free to change it. 
 #Be warned though, changing this rm -rf's your life...........
 #I hope you feel comfortable being able to do things in your own home :)
-todopy_dir=~/.todo
+directory=~/.todo
 
-if [ -d "$todopy_dir" ]
+if [ -d "$directory" ]
 then echo "You already have a .todo dir in Home! Either remove it or change path in setup.sh"
     echo "PS im located in ${PWD} :)"
 fi
 
-if [ ! -d "$todopy_dir" ]
-then mkdir "$todopy_dir"
+if [ ! -d "$directory" ]
+then mkdir "$directory"
 fi
+
+export directory
