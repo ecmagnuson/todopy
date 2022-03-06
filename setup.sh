9e0bad042f4082cc74e6d5d4fbe8e5b3299491f3
Elliott Magnuson <ecmagnuson@wisc.edu>
2022-03-05 20:09:17 -0600

    got y to work

--- setup.sh
+++ setup.sh
@@ -4,19 +4,24 @@
 #Be warned though, changing this rm -rf's your life...........
 #I hope can feel comfortable being able to do things in your home :)
 
-echo "do you want to make the todopy directory in the default path: ~/.todopy? [y/n]"
+todo_dir=/home/"$USER"/.todopy
 
+echo -n "Want me to pick the default location for the todopy directory (~/.todopy) [y/n]? "
+read answer
+
+if [ "$answer" != "${answer#[Yy]}" ] ; then 
+    if [ -d "$todo_dir" ]
+        then echo "You already have a .todo dir in Home! Either remove it or change path in setup.sh"
+        echo "PS im located in ${PWD} :)"
+    fi
+
+    if [ ! -d "$todo_dir" ] 
+        echo "I'm glad you feel comfortable being able to do things in your home"
+        then mkdir "$todo_dir"
+        touch ${todo_dir}/todo.txt ${todo_dir}/done.txt
+    fi
+fi
 
 #todo_dir=$1
 
 #todo_dir=/home/"$USER"/.todopy
-
-if [ -d "$todo_dir" ]
-then echo "You already have a .todo dir in Home! Either remove it or change path in setup.sh"
-    echo "PS im located in ${PWD} :)"
-fi
-
-if [ ! -d "$todo_dir" ]
-then mkdir "$todo_dir"
-     touch ${todo_dir}/todo.txt ${todo_dir}/done.txt
-fi
