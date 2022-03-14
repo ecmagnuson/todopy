ca87e5a000724b9703e08d5397af8dac42b8788e
Elliott Magnuson <ecmagnuson@wisc.edu>
2022-03-13 21:49:16 -0500

    try to remove args

--- todo.py
+++ todo.py
@@ -3,14 +3,10 @@
 from sys import argv
 import todo_cfg
 
-def write_str(file, string):
+def write_txt(file, string):
     with open(file, 'a') as file:
         file.write(string + '\n')
-
-def write_txt(file, *args):
-    with open(file, 'a') as file:
-        file.write(' '.join(argv[2:]) + '\n')  
-                      
+                 
 def read_txt(file):           
     with open(file, 'r') as file: 
         for i, line in enumerate(file.readlines(), 1):
@@ -20,17 +16,23 @@ def read_line(file, line_num):
     with open(file, 'r') as file:
         return file.readlines()[line_num - 1]
 
+def remove_line(file, line_num):
+    line = read_line(file, line_num)
+    return line
+
 if __name__ == '__main__':
     todo_txt = todo_cfg.todo_txt
     done_txt = todo_cfg.done_txt
 
+    print(remove_line(todo_txt, 1))
+
     if len(argv) == 1: #argv 0 is the py file itself
         print("'mansss todopy' for usage")        
     elif argv[1] == 'a' or argv[1] == 'add':
-        write_txt(todo_txt, argv[1:])    
+        task = ' '.join(argv[2:])
+        write_txt(todo_txt, task)    
     elif argv[1] == 'l' or argv[1] == 'ls':
         read_txt(todo_txt)
     elif argv[1] == 'd' or argv[1] == 'do':
         line = read_line(todo_txt, int(argv[2]))
-        print(line)
-        write_str(done_txt, line)
+        write_txt(done_txt, line)
