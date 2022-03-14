3987dfcef31a57477ca81389d607c6f4bd6501f7
Elliott Magnuson <ecmagnuson@wisc.edu>
2022-03-13 22:36:29 -0500

    combined two methods

--- todo.py
+++ todo.py
@@ -4,6 +4,7 @@ from sys import argv
 import todo_cfg
 
 def write_txt(file, string):
+    '''Write text to file with newline.'''
     with open(file, 'a') as file:
         file.write(string + '\n')
                  
@@ -17,12 +18,19 @@ def read_txt(file, *args):
         if len(args) == 0: 
             for i, line in enumerate(file.readlines(), 1):
                 print(i, line, end = '') #TODO fix the end = ''
-            return 
-        return file.readlines()[args[0] - 1]
+            return #better way to do this?
+        return file.readlines()[args[0] - 1].replace("\n", "") #TODO fix this ugliness. 
+        #I need to make a \n for writing to file, but i have to 
+        # manually remove here to transfer it to done.txt, which is clunky.
+
+def truncate_txt(file):
+    return
 
 def remove_line(file, line_num):
     line = read_txt(file, line_num)
     return line
+
+#t do multiple at a time.
 
 if __name__ == '__main__':
     todo_txt = todo_cfg.todo_txt
@@ -37,4 +45,5 @@ if __name__ == '__main__':
         read_txt(todo_txt)
     elif argv[1] == 'd' or argv[1] == 'do':
         line = read_txt(todo_txt, int(argv[2]))
+        print(line)
         write_txt(done_txt, line)
