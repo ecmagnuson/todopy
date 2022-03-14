9afc7b2072c3ccefb85f3382601aa96ac69a0244
Elliott Magnuson <ecmagnuson@wisc.edu>
2022-03-13 19:39:30 -0500

    fixed write new line

--- todo.py
+++ todo.py
@@ -5,7 +5,7 @@ import todo_cfg
 
 def write_txt(file, *args):
     with open(file, 'a') as file: #a instead of w is append :)
-        return file.write(' '.join(argv[2:]))                         #TODO new line
+        file.write(' '.join(argv[2:]) + '\n')                         #TODO new line
             
 def read_txt(file):                                                  #TODO get index to work
     with open(file, 'r') as file:
@@ -17,13 +17,10 @@ if __name__ == '__main__':
     done_txt = todo_cfg.done_txt
 
     if len(argv) == 1: #argv 0 is the py file itself
-        print("'man todopy' for usage")
-            
+        print("'man todopy' for usage")        
     elif argv[1] == 'a' or argv[1] == 'add':
-        print(write_txt(todo_txt, argv[1:]))
-        
+        write_txt(todo_txt, argv[1:])    
     elif argv[1] == 'l' or argv[1] == 'ls':
         print(read_txt(todo_txt))
-
     else:
         print("'man todopy' for usage")
