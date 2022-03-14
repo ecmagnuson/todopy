afbd68696b5a983e4522cfd8a9aab50ee8ad6150
Elliott Magnuson <ecmagnuson@wisc.edu>
2022-03-13 21:24:49 -0500

    read lines index working

--- todo.py
+++ todo.py
@@ -4,24 +4,28 @@ from sys import argv
 import todo_cfg
 
 def write_txt(file, *args):
-    with open(file, 'a') as file: #a instead of w is append :)
+    with open(file, 'a') as file:
         file.write(' '.join(argv[2:]) + '\n')  
                       
 def read_txt(file):           
+    with open(file, 'r') as file: 
+        for i, line in enumerate(file.readlines(), 1):
+            print(i, line, end = '') #TODO fix the end = ''
+
+def read_line(file, line_num):
     with open(file, 'r') as file:
-        lines  = file.readlines()
-    for i, line in enumerate(lines):
-        print(i, line, end = '') #TODO fix the end = ''
-  
+        return file.readlines()[line_num - 1]
+
 if __name__ == '__main__':
     todo_txt = todo_cfg.todo_txt
     done_txt = todo_cfg.done_txt
 
     if len(argv) == 1: #argv 0 is the py file itself
-        print("'man todopy' for usage")        
+        print("'mansss todopy' for usage")        
     elif argv[1] == 'a' or argv[1] == 'add':
         write_txt(todo_txt, argv[1:])    
     elif argv[1] == 'l' or argv[1] == 'ls':
         read_txt(todo_txt)
-    else:
-        print("'man todopy' for usage")
+    elif argv[1] == 'd' or argv[1] == 'do':
+        line = read_line(todo_txt, int(argv[2]))
+        print(line)
