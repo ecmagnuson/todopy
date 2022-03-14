030a5212185083e4627f26225270f155aeef278d
Elliott Magnuson <ecmagnuson@wisc.edu>
2022-03-13 22:20:34 -0500

    combined two methods

--- todo.py
+++ todo.py
@@ -7,32 +7,34 @@ def write_txt(file, string):
     with open(file, 'a') as file:
         file.write(string + '\n')
                  
-def read_txt(file):           
+def read_txt(file, *args):   
+    ''' This function will read text from a text file. If there are no args
+    passed into the function it will print all the contents of the file.
+    If there is an argument passed in it will return only the str on the
+    row passed into the function. Starting at index 1.
+    '''        
     with open(file, 'r') as file: 
-        for i, line in enumerate(file.readlines(), 1):
-            print(i, line, end = '') #TODO fix the end = ''
-
-def read_line(file, line_num):
-    with open(file, 'r') as file:
-        return file.readlines()[line_num - 1]
+        if len(args) == 0: 
+            for i, line in enumerate(file.readlines(), 1):
+                print(i, line, end = '') #TODO fix the end = ''
+            return 
+        return file.readlines()[args[0] - 1]
 
 def remove_line(file, line_num):
-    line = read_line(file, line_num)
+    line = read_txt(file, line_num)
     return line
 
 if __name__ == '__main__':
     todo_txt = todo_cfg.todo_txt
     done_txt = todo_cfg.done_txt
 
-    print(remove_line(todo_txt, 1))
-
     if len(argv) == 1: #argv 0 is the py file itself
-        print("'mansss todopy' for usage")        
+        print("'man todopy' for usage")        
     elif argv[1] == 'a' or argv[1] == 'add':
         task = ' '.join(argv[2:])
         write_txt(todo_txt, task)    
     elif argv[1] == 'l' or argv[1] == 'ls':
         read_txt(todo_txt)
     elif argv[1] == 'd' or argv[1] == 'do':
-        line = read_line(todo_txt, int(argv[2]))
+        line = read_txt(todo_txt, int(argv[2]))
         write_txt(done_txt, line)
