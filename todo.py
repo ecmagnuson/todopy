62de9599994c30fe3e9e5ba52c6e5e25de41660c
Elliott Magnuson <ecmagnuson@wisc.edu>
2022-03-14 23:13:53 -0500

    backup

--- todo.py
+++ todo.py
@@ -11,20 +11,21 @@ def write_txt(file, string):
 def read_all_lines(file):   
     '''Reads all the lines from a file'''  
     with open(file, 'r') as file: 
-            for i, line in enumerate(file.readlines(), 1):
-                print(i, line, end = '') #TODO fix the end = ''
+        return file.readlines()
 
 def read_line(file, line):
     '''Reads one or more lines from a file'''
     with open(file, 'r') as file: 
-        return file.readlines()[line - 1].replace("\n", "")
+        return file.readlines()[line - 1]
 
-
-
-def remove_line(file, line_num):
-    line = read_all_lines(file, line_num)
-    return line
-
+def remove_line(file, string):
+    '''Removed a line from a file'''
+    all_txt = read_all_lines(file)
+    line_to_remove = read_line(file, string)
+    with open(file, 'w') as file:
+        for line in all_txt:
+            if line != line_to_remove:
+                file.write(line)
 
 if __name__ == '__main__':
     todo_txt = todo_cfg.todo_txt
@@ -33,10 +34,13 @@ if __name__ == '__main__':
     if len(argv) == 1: #argv 0 is the py file itself
         print("'man todopy' for usage") 
     elif argv[1] == 'l' or argv[1] == 'ls':
-        read_all_lines(todo_txt)   
+        for i, line in enumerate(read_all_lines(todo_txt), 1):
+            print(i, line.replace("\n", ""))
+                  
     elif argv[1] == 'a' or argv[1] == 'add':
         task = ' '.join(argv[2:])
         write_txt(todo_txt, task)    
     elif argv[1] == 'd' or argv[1] == 'do':
         line = read_line(todo_txt, int(argv[2]))
-        write_txt(done_txt, line)
+        write_txt(done_txt, line.replace("\n", ""))
+        remove_line(todo_txt, int(argv[2]))
