ff7e271e6d6ead63dfa70ad177c1005c13228d0a
Elliott Magnuson <ecmagnuson@wisc.edu>
2022-03-14 22:56:06 -0500

    backup

--- todo.py
+++ todo.py
@@ -4,47 +4,39 @@ from sys import argv
 import todo_cfg
 
 def write_txt(file, string):
-    '''Write text to file with newline.'''
+    '''Write one line to file with newline.'''
     with open(file, 'a') as file:
         file.write(string + '\n')
                  
-def read_txt(file, *args):   
-    ''' This function will read text from a text file. If there are no args
-    passed into the function it will print all the contents of the file.
-    If there is an argument passed in it will return only the str on the
-    row passed into the function. Starting at index 1.
-    '''        
+def read_all_lines(file):   
+    '''Reads all the lines from a file'''  
     with open(file, 'r') as file: 
-        if len(args) == 0: 
             for i, line in enumerate(file.readlines(), 1):
                 print(i, line, end = '') #TODO fix the end = ''
-            return #better way to do this?
-        return file.readlines()[args[0] - 1].replace("\n", "") #TODO fix this ugliness. 
-        #I need to make a \n for writing to file, but i have to 
-        # manually remove here to transfer it to done.txt, which is clunky.
+
+def read_line(file, line):
+    '''Reads one or more lines from a file'''
+    with open(file, 'r') as file: 
+        return file.readlines()[line - 1].replace("\n", "")
 
 
-
-def truncate_txt(file):
-    return
 
 def remove_line(file, line_num):
-    line = read_txt(file, line_num)
+    line = read_all_lines(file, line_num)
     return line
 
-#t do multiple at a time.
 
 if __name__ == '__main__':
     todo_txt = todo_cfg.todo_txt
     done_txt = todo_cfg.done_txt
 
     if len(argv) == 1: #argv 0 is the py file itself
-        print("'man todopy' for usage")        
+        print("'man todopy' for usage") 
+    elif argv[1] == 'l' or argv[1] == 'ls':
+        read_all_lines(todo_txt)   
     elif argv[1] == 'a' or argv[1] == 'add':
         task = ' '.join(argv[2:])
         write_txt(todo_txt, task)    
-    elif argv[1] == 'l' or argv[1] == 'ls':
-        read_txt(todo_txt)
     elif argv[1] == 'd' or argv[1] == 'do':
-        line = read_txt(todo_txt, int(argv[2]))
+        line = read_line(todo_txt, int(argv[2]))
         write_txt(done_txt, line)
